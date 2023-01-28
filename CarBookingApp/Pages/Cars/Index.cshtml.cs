d129e4ff59876c828975ae1c15ed4d13dc9ea9fd
A-San96 <assanediatta96@gmail.com>
2023-01-28 20:58:19 +0000

    Add project files.

---
+++ CarBookingApp/Pages/Cars/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarBookingApp.Pages.Cars
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
