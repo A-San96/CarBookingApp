5bfa6603698f4b1e678ee945c093e7105602ca52
Assane DIATTA <assanediatta96@gmail.com>
2023-01-31 16:17:27 +0000

    add data layer

---
+++ CarBookingApp.Data/CarBookAppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookingApp.Data
+{
+    public class CarBookAppDbContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+    }
+}
