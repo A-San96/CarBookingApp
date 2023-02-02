07e33bc32fad7cba713c807e0281683a94a8ca83
A-San96 <assanediatta96@gmail.com>
2023-02-02 19:55:59 +0000

    Scaffold Car class on Data Layer

--- CarBookingApp/Pages/Cars/Index.cshtml.cs
+++ CarBookingApp/Pages/Cars/Index.cshtml.cs
@@ -1,22 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CarBookingApp.Data;
 
 namespace CarBookingApp.Pages.Cars
 {
     public class IndexModel : PageModel
     {
-        private readonly IConfiguration _configuration;
+        private readonly CarBookingApp.Data.CarBookingAppDbContext _context;
 
-        public IndexModel(IConfiguration configuration)
+        public IndexModel(CarBookingApp.Data.CarBookingAppDbContext context)
         {
-            this._configuration = configuration;
+            _context = context;
         }
-        public string Heading { get; set; }
-        public string Message { get; set; }
-        public void OnGet()
+
+        public IList<Car> Car { get;set; } = default!;
+
+        public async Task OnGetAsync()
         {
-            Heading = "List of Cars";
-            Message = _configuration["Message"];
+            if (_context.Cars != null)
+            {
+                Car = await _context.Cars.ToListAsync();
+            }
         }
     }
 }
