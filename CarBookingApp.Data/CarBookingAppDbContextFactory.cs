564f0a8847708a4ff471fb7aa38bf7f54010e7ef
A-San96 <assanediatta96@gmail.com>
2023-02-02 19:52:29 +0000

    add dbcontext factory for fix bug on scaffolding Car

---
+++ CarBookingApp.Data/CarBookingAppDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookingApp.Data
+{
+    public class CarBookingAppDbContextFactory : IDesignTimeDbContextFactory<CarBookingAppDbContext>
+    {
+        
+        public CarBookingAppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CarBookingAppDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CarBookingApp_db;Trusted_Connection=True;MultipleActiveResultSets=True");
+
+            return new CarBookingAppDbContext(optionsBuilder.Options);
+        }
+    }
+}
