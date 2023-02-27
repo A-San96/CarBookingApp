1106c7226b0d4c14bbd42226816fa51f33795bb1
A-San96 <assanediatta96@gmail.com>
2023-02-27 11:59:50 +0000

    Add edit car features

---
+++ CarBookingApp/Pages/Cars/Update.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CarBookingApp.Data;
+
+namespace CarBookingApp.Pages.Cars
+{
+    public class UpdateModel : PageModel
+    {
+        private readonly CarBookingApp.Data.CarBookingAppDbContext _context;
+
+        public UpdateModel(CarBookingApp.Data.CarBookingAppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Car Car { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Cars == null)
+            {
+                return NotFound();
+            }
+
+            var car =  await _context.Cars.FirstOrDefaultAsync(m => m.Id == id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            Car = car;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Car).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarExists(Car.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool CarExists(int id)
+        {
+          return _context.Cars.Any(e => e.Id == id);
+        }
+    }
+}
